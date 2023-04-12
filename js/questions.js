41f4bc481a3ee8f7fed66b11b65794a978842fe4
Arnold-04 <arnoldkwame1@gmail.com>
2023-04-12 21:03:49 +0000

    QUIZ APP

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+
+
+
+let questions = [{
+  numb: 1,
+  question: "What does HTML stand for?",
+  answer: "Hyper Text Markup Language", options: [
+    "Hyper Text Preprocessor", 
+    "Hyper Text Markup Language", 
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+{
+  numb: 2,
+  question: "How to create an ordered list in HTML?",
+  answer: "<ol>",
+  options: [
+    "<ol>",
+    "<ul>",
+    "<a href>",
+    "None of the above"
+  ]
+},
+{
+  numb: 3,
+  question: "What are the types of lists available in HTML?",
+  answer: "Ordered, Unordered",
+  options: [
+    "Ordered, Unordered",
+    "Bulleted, Numbered Lists",
+    "Named, Unnamed list",
+    "None of the above"
+  ]
+},
+{
+  numb: 4,
+  question: "How many sizes of headers are available in HTML by default?",
+  answer: "6",
+  options: [
+    "3",
+    "4",
+    "5",
+    "6"
+  ]
+},
+{
+  numb: 5,
+  question: "What does XML stand for?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]
+},
+{
+  numb: 6,
+  question: "What does HTTP stand for?",
+  answer: "Hypertext Transfer Protocol",
+  options: [
+    "Hypertext Transfer Protocol",
+    "Hypertext Translation Protocol",
+    "Hypertext Transmit Protocol",
+    "Hypertext Transaction Protocol"
+  ]
+},
+{
+  numb: 7,
+  question: "HTML files are saved by default with the extension?",
+  answer: ".html",
+  options: [
+    ".html",
+    ".txt",
+    ".js",
+    ".h"
+  ]
+},
+{
+  numb: 8,
+  question: "We enclose HTML tags within?",
+  answer: "<>",
+  options: [
+    "<>",
+    "{}",
+    "()",
+    "[]"
+  ]
+},
+{
+  numb: 9,
+  question: "What does DNS stand for?",
+  answer: "Domain Name System",
+  options: [
+    "Domain Name Service",
+    "Domain Name System",
+    "Domain Name Server",
+    "Domain Name Security"
+  ]
+},
+{
+  numb: 10,
+  question: "How to display preformatted text in HTML?",
+  answer: "<pre>",
+  options: [
+    "<pre>",
+    "<p>",
+    "<hr>",
+    "<br>"
+  ]
+}
+];
